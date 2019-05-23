089196c816003e5b04ac291e024a790d95f9bf42
haroldcamacho <haroldcj95@gmail.com>
2019-05-22 23:50:16 -0400

    add review_mailer test

--- spec/mailers/review_mailer_spec.rb
+++ spec/mailers/review_mailer_spec.rb
@@ -14,18 +14,17 @@ RSpec.describe ReviewMailer, type: :mailer do
     expect(ActionMailer::Base.deliveries.count).to eq(0)
   end
 
-  it 'should not proceed if there is no event' do
-    # tutor_user = User.create(username: "test", email: "123@gmail.com", password: "123123", id: 123)
-    # relator_user = User.create(username: "test2", email: "1234@gmail.com", password: "123123", id: 12)
-    # director_user = User.create(username: "test3", email: "1235@gmail.com", password: "123123", id: 32)
-
-    # @paper_assignment = Paper.new(tutor_id: 123, relator_id: 12, director_id: 32)
-
-
-    # paper_assignment_hash = { tutor_id: 123, relator_id: 12, director_id: 32 }
-    # paper_assignment = Paper.create(tutor_id: 123, relator_id: 12, director_id: 32)
-    # ReviewMailer.review_notification("test@gmail.com", @paper_assignment, "paper", nil).deliver_now
-    # expect(ActionMailer::Base.deliveries.count).to eq(0)
+  it 'should send and email when the requirements are satisifed' do
+    test_assignment = Assingment.create(tutor_id: 123, relator_id: 12, director_id: 32)
+    tutor_user = User.create(username: "test", email: "123@gmail.com", password: "123123", id: 123)
+    relator_user = User.create(username: "test2", email: "1234@gmail.com", password: "123123", id: 12)
+    director_user = User.create(username: "test3", email: "1235@gmail.com", password: "123123", id: 32)
+    current_user = User.create(username: "test5", email: "12356@gmail.com", password: "123123", id: 323, name: "Harold", lastname: "Camacho")
+    current_user.complete_name()
+    paper_test = Paper.create(id:1, title: "Test", user: current_user)
+    event_test = Event.create(id: 1)
+    ReviewMailer.review_notification(current_user, test_assignment, paper_test, event_test).deliver_now
+    expect(ActionMailer::Base.deliveries.count).to eq(1)
   end
 
   after(:each) do
