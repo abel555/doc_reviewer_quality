70b118b64501d0abddea603aa487e57c67d34319
haroldcamacho <haroldcj95@gmail.com>
2019-05-22 23:59:37 -0400

    add assignment and paper mailers test

--- spec/mailers/assignment_mailer_spec.rb
+++ spec/mailers/assignment_mailer_spec.rb
@@ -1,5 +1,26 @@
 require "rails_helper"
 
 RSpec.describe AssignmentMailer, type: :mailer do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before(:each) do
+    ActionMailer::Base.delivery_method = :test
+    ActionMailer::Base.perform_deliveries = true
+    ActionMailer::Base.deliveries = []
+  end
+
+  it 'should send and email when the requirements are satisifed' do
+    test_assignment = Assingment.create(tutor_id: 123, relator_id: 12, director_id: 32)
+    tutor_user = User.create(username: "test", email: "123@gmail.com", password: "123123", id: 123)
+    relator_user = User.create(username: "test2", email: "1234@gmail.com", password: "123123", id: 12)
+    director_user = User.create(username: "test3", email: "1235@gmail.com", password: "123123", id: 32)
+    current_user = User.create(username: "test5", email: "12356@gmail.com", password: "123123", id: 323, name: "Harold", lastname: "Camacho")
+    current_user.complete_name()
+    paper_test = Paper.create(id:1, title: "Test", user: current_user)
+    event_test = Event.create(id: 1)
+    AssignmentMailer.tribunal_confirmation(test_assignment, paper_test, event_test).deliver_now
+    expect(ActionMailer::Base.deliveries.count).to eq(1)
+  end
+
+  after(:each) do
+    ActionMailer::Base.deliveries.clear
+  end
 end
--- spec/mailers/paper_mailer_spec.rb
+++ spec/mailers/paper_mailer_spec.rb
@@ -7,10 +7,13 @@ RSpec.describe PaperMailer, type: :mailer do
     ActionMailer::Base.deliveries = []
   end
 
-  it 'should not proceed if email is empty' do
-    mail_user = User.create(username: "test", email: "123@gmail.com", password: "123123")
-    PaperMailer.status_notification(mail_user)
-    expect(ActionMailer::Base.deliveries.count).to eq(0)
+  it 'should send and email when the requirements are satisifed' do
+    current_user = User.create(username: "test5", email: "12356@gmail.com", password: "123123", id: 323, name: "Harold", lastname: "Camacho")
+    current_user.complete_name()
+    paper_test = Paper.create(id:1, title: "Test", user: current_user)
+    event_test = Event.create(id: 1)
+    PaperMailer.status_notification(current_user, paper_test, event_test).deliver_now
+    expect(ActionMailer::Base.deliveries.count).to eq(1)
   end
 
   after(:each) do
